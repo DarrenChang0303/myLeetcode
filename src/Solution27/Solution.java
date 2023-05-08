d2a2942c9ce64a07db7b996168b9bb58f146ca93
Darren <change2014@outlook.com>
2023-05-08 18:16:50 +0800

    update solution 27

--- src/Solution27/Solution.java
+++ src/Solution27/Solution.java
@@ -1,4 +1,16 @@
 package Solution27;
 
 public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int point = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[point] = nums[i];
+                point++;
+            }
+        }
+        int k = point;
+        return k;
+    }
 }
